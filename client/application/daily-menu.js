ab0e424a2b1470be89ca454d4152181f1e1a3a06
Marko Novak <marko.novak@zemanta.com>
2015-10-16 16:47:33 +0200

    Reorganized the code.

---
+++ client/application/daily-menu.js
@@ -0,0 +1,26 @@
+// This code only runs on the client
+Template.body.helpers({
+    menu_items: function () {
+        return MenuItems.find({}, {sort: {createdAt: -1}});
+    }
+});
+
+Template.body.events({
+    "submit .new-menu-item": function (event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // Get value from form element
+        var text = event.target.text.value;
+
+        // Insert a task into the collection
+        Meteor.call("addMenuItem", text)
+
+        // Clear form
+        event.target.text.value = "";
+    }
+});
+
+Accounts.ui.config({
+    //passwordSignupFields: "USERNAME_ONLY"
+});
---
+++ client/model/restaurant.js
@@ -0,0 +1,7 @@
+Template.restaurant.events({
+    'click .add': function(e) {
+        e.preventDefault();
+
+        $('#animalsModal').modal('show');
+    }
+});
